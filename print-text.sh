8d56aa5f1c0c103301e1015599c6353b20402b25
kenjara <kenjara@hotmail.com>
2018-01-19 07:31:22 +0000

    Moved commands to correct places

--- print-text.sh
+++ print-text.sh
@@ -1,1 +1,3 @@
+stty -F /dev/serial0 19200
+#stty -F /dev/serial0 9600
 echo $1\\n\\n\\n > /dev/serial0
--- serial-setup.sh
+++ serial-setup.sh
@@ -1,5 +1,3 @@
-stty -F /dev/serial0 19200
-#stty -F /dev/serial0 9600
 sudo chmod 777 /dev/serial0
 sudo chown pi:pi /dev/serial0
 
